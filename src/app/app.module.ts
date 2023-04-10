6b4d93b19237fab057b2338c4949c8027867edef
saralazic <sara.lazic@outlook.com>
2023-04-10 22:44:25 +0200

    added angular materialize, only icons left for this phase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/authorize/register/register.component';
 import { LoginComponent } from './components/authorize/login/login.component';
@@ -10,6 +11,7 @@ import { HeaderComponent } from './components/marginals/header/header.component'
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { FooterComponent } from './components/marginals/footer/footer.component';
 import { AboutComponent } from './components/about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,14 @@ import { AboutComponent } from './components/about/about.component';
     FooterComponent,
     AboutComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
