ee5b4a89dd19e8fd898eaf1454df67b7b64150f9
saralazic <sara.lazic@outlook.com>
2023-04-15 14:07:52 +0200

    add page without ticket counts

--- src/app/components/customer-tickets/children/children.component.ts
+++ src/app/components/customer-tickets/children/children.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-children',
   templateUrl: './children.component.html',
-  styleUrls: ['./children.component.css']
+  styleUrls: ['./children.component.css'],
 })
 export class ChildrenComponent {
+  public promocode: string | undefined;
 
+  constructor() {}
+
+  ngOnInit() {}
 }
