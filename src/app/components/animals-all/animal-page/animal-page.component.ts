49f4bf7e1d0d80432943dde082c65a0f6f0f0118
saralazic <sara.lazic@outlook.com>
2023-04-26 00:13:10 +0200

    current progress

--- src/app/components/animals-all/animal-page/animal-page.component.ts
+++ src/app/components/animals-all/animal-page/animal-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ANIMAL_ERROR } from 'src/app/constants/errors';
+import { Animal } from 'src/app/models/animal';
+import { animalService } from 'src/app/services/animal.service';
 
 @Component({
   selector: 'app-animal-page',
   templateUrl: './animal-page.component.html',
-  styleUrls: ['./animal-page.component.css']
+  styleUrls: ['./animal-page.component.css'],
 })
 export class AnimalPageComponent {
+  private animalService = animalService;
+  public error?: string;
+  public animal: Animal | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      console.log(params); // Object containing the query parameters
+      console.log(params['index']); // Accessing a specific query parameter\
+      const index = params['index'];
+      if (index) {
+        this.animal = this.animalService.getAnimalByIndex(index);
+      } else this.error = ANIMAL_ERROR;
+    });
+  }
 }
--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -8,6 +8,13 @@ export class AnimalService {
     localStorage.setItem(localStorageItems.ANIMALS, JSON.stringify(animals));
   }
 
+  public getAnimalByIndex(index: number): Animal | null {
+    if (index < 0) return null;
+    const allAnimals = this.getAnimals();
+    if (index >= allAnimals.length) return null;
+    return allAnimals[index];
+  }
+
   public getAnimals() {
     const animals = localStorage.getItem(localStorageItems.ANIMALS);
     return animals ? JSON.parse(animals) : [];
