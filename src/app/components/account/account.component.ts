b71dfb1d686111186e5f8238825766dab0c21c82
saralazic <sara.lazic@outlook.com>
2023-05-03 22:30:07 +0200

    current progress

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,10 +1,96 @@
 import { Component } from '@angular/core';
+import { RegisterErrors } from 'src/app/constants/errors';
+import { UserData, UserWithType } from 'src/app/models/user';
+import { authService } from 'src/app/services/auth.service';
+import {
+  emailValid,
+  emptyFields,
+  otherFieldsValid,
+  passwordValid,
+  phoneValid,
+  returnEmptyObject,
+} from 'src/app/utilities';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
 export class AccountComponent {
+  private authService = authService;
+  public loggedIn: UserWithType;
 
+  username: string | undefined;
+  password: string | undefined;
+  email: string | undefined;
+  firstName: string | undefined;
+  lastName: string | undefined;
+  phone: string | undefined;
+  address: string | undefined;
+
+  error: string | undefined;
+
+  constructor() {
+    const logged = this.authService.getLoggedUser();
+    this.loggedIn = logged ?? returnEmptyObject();
+
+    this.username = this.loggedIn.username;
+    this.password = this.loggedIn.password;
+    this.email = this.loggedIn.email;
+    this.firstName = this.loggedIn.first_name;
+    this.lastName = this.loggedIn.last_name;
+    this.phone = this.loggedIn.phone;
+    this.address = this.loggedIn.address;
+
+    console.log(this.loggedIn);
+  }
+
+  saveChange() {
+    this.error = undefined;
+    const data = {
+      username: this.username,
+      password: this.password,
+      email: this.email,
+      first_name: this.firstName,
+      last_name: this.lastName,
+      phone: this.phone,
+      address: this.address,
+    } as UserData;
+
+    this.validateInput(data);
+
+    if (!this.error) {
+      const authServiceResponse = this.authService.register(data);
+      if (typeof authServiceResponse === 'string') {
+        this.error = authServiceResponse;
+      } else {
+        //this.router.navigate(['/homepage']);
+      }
+    }
+  }
+
+  private validateInput(data: UserData): void {
+    if (emptyFields(data)) this.error = RegisterErrors.EMPTY_INPUT;
+
+    if (!emailValid(this.email ?? ''))
+      this.error = RegisterErrors.INVALID_EMAIL;
+
+    if (!phoneValid(this.phone ?? ''))
+      this.error = RegisterErrors.INVALID_PHONE;
+
+    if (!passwordValid(this.password ?? ''))
+      this.error = RegisterErrors.INVALID_PASSWORD;
+
+    if (
+      !(
+        otherFieldsValid(this.firstName ?? '') &&
+        otherFieldsValid(this.lastName ?? '') &&
+        otherFieldsValid(this.address ?? '') &&
+        otherFieldsValid(this.username ?? '')
+      )
+    )
+      this.error = RegisterErrors.INVALID_FIELD;
+  }
+
+  return() {}
 }
--- src/app/utilities.ts
+++ src/app/utilities.ts
@@ -1,7 +1,61 @@
+import { UserData, UserType, UserWithType } from './models/user';
+
 export function reloadPage() {
   window.location.reload();
 }
 
 export async function sleep(ms: number): Promise<any> {
   return new Promise((r) => setTimeout(r, ms));
+}
+
+export function passwordValid(pass: string): boolean {
+  return (
+    /^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/.test(pass) &&
+    /^[a-zA-Z]/.test(pass)
+  );
+}
+
+export function emailValid(email: string): boolean {
+  return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+    email
+  );
+}
+
+export function phoneValid(phone: string): boolean {
+  return (
+    /^(060|061|062|063|066|067)\d{7}$/.test(phone) || /^(0)\d{8}$/.test(phone)
+  );
+}
+
+export function otherFieldsValid(field: string): boolean {
+  if (field.length < 3) return false;
+  return true;
+}
+
+export function emptyFields(data: UserData): boolean {
+  if (
+    typeof data.username !== 'string' ||
+    typeof data.password !== 'string' ||
+    typeof data.email !== 'string' ||
+    typeof data.first_name !== 'string' ||
+    typeof data.last_name != 'string' ||
+    typeof data.address !== 'string' ||
+    typeof data.phone !== 'string'
+  ) {
+    return true; //error
+  }
+  return false;
+}
+
+export function returnEmptyObject(): UserWithType {
+  return {
+    username: '',
+    password: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+    phone: '',
+    address: '',
+    type: UserType.visitor,
+  } as UserWithType;
 }
